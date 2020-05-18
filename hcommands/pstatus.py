0cea9dd6094ac9c413f1d4c30e446536a43b7b85
iDeagan <63136371+iDeagan@users.noreply.github.com>
2020-05-18 15:34:41 -0700

    Added last game ended time if the player is online

--- hcommands/pstatus.py
+++ hcommands/pstatus.py
@@ -4,6 +4,7 @@ from datetime import datetime, timezone, timedelta
 import time
 
 from hmisc import API_KEY
+from hmisc import pcache
 
 APIKEY = API_KEY.return_api_key()
 
@@ -18,11 +19,15 @@ def get_session(name):
 
     url2 = f"https://api.hypixel.net/status?key={APIKEY}&uuid=" + returnUuid
     url3 = f"https://api.hypixel.net/player?key={APIKEY}&name={name}"
+    url4 = f"https://api.hypixel.net/recentGames?key={APIKEY}&uuid=" + returnUuid
 
     res = requests.get(url2)
     res2 = requests.get(url3)
+    res3 = requests.get(url4)
+
     data = res.json()
     data2 = res2.json()
+    data3 = res3.json()
 
     if data["session"] is None:
         return None
@@ -59,9 +64,13 @@ def get_session(name):
         game_mode = (data["session"]["mode"])
         log_in = int(data2["player"]["lastLogin"])
         log_out = int(data2["player"]["lastLogout"])
+        last_game_ended = int(data3["games"][0]["ended"])
+
+        last_game_ended_converted = timedate = time.strftime('%Y-%m-%d\n%I:%M %p', time.localtime(last_game_ended / 1000))
+
         log_in_converted = timedate = time.strftime('%Y-%m-%d\n%I:%M %p', time.localtime(log_in / 1000))
         log_out_converted = timedate = time.strftime('%Y-%m-%d\n%I:%M %p', time.localtime(log_out / 1000))
 
 
 
-        return onlinestatus_string + "`" + str(onlinestatus) + "`" + newLinestring + gameType_string + "`" + game_type + "`" + newLinestring + gameMode_string + "`" + game_mode + "`" + newLinestring + newLinestring + lastLogin_string + "`" + str(log_in_converted) + "`" + newLinestring + lastLogout_string + "`" + str(log_out_converted) + "`"
+        return onlinestatus_string + "`" + str(onlinestatus) + "`" + newLinestring + gameType_string + "`" + game_type + "`" + newLinestring + gameMode_string + "`" + game_mode + "`" + newLinestring + newLinestring + lastLogin_string + "`" + str(log_in_converted) + "`" + newLinestring + lastLogout_string + "`" + str(log_out_converted) + "`" + newLinestring + newLinestring + "Last Game Ended: " + "`" + str(last_game_ended_converted) + "`"
