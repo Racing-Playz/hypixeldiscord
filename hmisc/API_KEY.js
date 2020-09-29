d558ce5866deabea4639737e951511fc6821a13d
RacingPlayz <72081031+Racing-Playz@users.noreply.github.com>
2020-09-29 17:31:34 -0400

    Update and rename API_KEY.py to API_KEY.js

--- hmisc/API_KEY.js
+++ hmisc/API_KEY.js
@@ -8,7 +8,7 @@ import random
 #one api key should be enough, but if you get api exceeded errors add another one (by going on an alt and typing /api in the chat).
 
 def return_api_key():
-    API_KEY = [""]
+    API_KEY = ["Y8bb07638-1de4-46e7-8133-af9bfb0e67f9"]
     return random.choice(API_KEY)
     #This just chooses a random api key
     #for each request to prevent throttling
